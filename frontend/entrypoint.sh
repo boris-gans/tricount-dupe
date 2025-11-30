e415d006ffd773500c0d4ac74ec64aa03f45c274
studentBorisGans <bgans.ieu2023@student.ie.edu>
2025-11-30 16:06:58 +0100

    fff

--- frontend/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Create runtime config file from environment variable
-echo "{
-  \"apiBaseUrl\": \"${vite-api-base-url}\"
-}" > /usr/share/nginx/html/config.json
-
-# Start Nginx
-exec nginx -g "daemon off;"
